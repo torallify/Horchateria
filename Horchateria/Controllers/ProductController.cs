2e1fc6faac56a25710f613e9f72085b90ea0042d
torallify <torallify@gmail.com>
2020-04-08 02:34:07 -0400

    updated Product Controller and added Menu to application

---
+++ Horchateria/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Horchateria.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Horchateria.Controllers
+{
+    public class ProductController : Controller
+    {
+        IConfiguration ConfigRoot;
+        SqlConnection connection;
+        public ProductController(IConfiguration config)
+        {
+            ConfigRoot = config;
+            connection = new SqlConnection(ConfigRoot.GetConnectionString("horchateriaDB"));
+        }
+
+        public IActionResult Index()
+        {
+            string queryString = "SELECT * FROM Products ORDER BY Category";
+            IEnumerable<Product> Products = connection.Query<Product>(queryString);
+
+            ViewData["Products"] = Products;
+            return View("ProductIndex");
+        }
+    }
+}
---
+++ Horchateria/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Horchateria.Models
+{
+    public class Product
+    {
+        private int id;
+        private string name;
+        private string description;
+        private double price;
+        private string category;
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public string Description { get => description; set => description = value; }
+        public double Price { get => price; set => price = value; }
+        public string Category { get => category; set => category = value; }
+    }
+}
